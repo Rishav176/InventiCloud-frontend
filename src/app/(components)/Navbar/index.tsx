a80770624517f64fb994f2a8b278422f33117cb9
Rishav Kumar <rishavkr1766@gmail.com>
2024-08-07 21:53:23 -0600

    update navbar add right section with link to settings

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { Bell, Menu, Sun } from 'lucide-react'
+import { Bell, Menu, Settings, Sun } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -11,14 +12,14 @@ const Navbar = () => {
         <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={()=>{}}>
           <Menu className='w-4 h-4 '/>
         </button>
-      </div>
+      
       <div className='relative'>
         <input type='search' placeholder='Start typing to search groups and products' className='pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500' />
         <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointet-events-none'>
           <Bell className='text-gray-500' size={20}/>
         </div>
       </div>
-
+      </div>
       <div className='flex justify-between items-center gap-5'>
         <div className='hidden md:flex justify-between items-center gap-5'>
           <div>
@@ -32,7 +33,17 @@ const Navbar = () => {
             3
           </span>
           </div>
+          <hr className='w-0 h-7 border-solid border-l border-gray-300 mx-3'/>
+          <div className='flex items-center gap-3 cursor-pointer'>
+            <div className='w-9 h-9'>
+              image
+            </div>
+            <span className='font-semibold'>Rishav Kumar</span>
+          </div>
         </div>
+        <Link href='/settings'>
+        <Settings className='cursor-pointer text-gray-500' size={24}/>
+        </Link>
       </div>
 
       </div>
