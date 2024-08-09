6ad1d7cba62fabda4c0df584c647869d8ea79dab
Rishav Kumar <rishavkr1766@gmail.com>
2024-08-08 22:53:33 -0600

    update sidebar add logo and footer

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,10 +1,30 @@
+'use client'
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 
 
 const Sidebar = () => {
+    let date=new Date()
+    let year=date.getFullYear()
   return (
-    <div>Sidebar</div>
+    <div>
+       <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+            <div>logo</div>
+            <h1 className='font-extrabold text-2xl'>InventiCloud</h1>
+        <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={()=>{}}>
+            <Menu className='w-4 h-4'/>
+        </button>
+        </div>
+
+        <div className='flex-grow mt-8'>
+
+        </div>
+
+        <div>
+            <p className='text-center text-xs text-gray-500'>&copy; {year} InventiCloud</p>
+        </div>
+    </div>
   )
 }
 
